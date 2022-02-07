a12626746dcc4f33a1f8aad4b248fdb9f922a9e4
大山和徳 <kazunori-oyama@tonichi-printing.co.jp>
2022-02-07 09:44:34 +0900

    情報をすべて出力

--- src/views/Issues.vue
+++ src/views/Issues.vue
@@ -39,6 +39,25 @@
             <v-card-title>{{issue.issueName}}</v-card-title>
             <v-card-text>
               <v-chip-group column>
+                <v-chip small text-small outlined class="mr-2" color="indigo" text-color="indigo">
+                  <v-avatar small left>
+                    <v-icon small>mdi-bullhorn-outline</v-icon>
+                  </v-avatar>
+                  {{issue.shareState}}
+                </v-chip>
+                <v-chip small text-small outlined class="mr-2" color="indigo" text-color="indigo">
+                  <v-avatar small left>
+                    <v-icon small>mdi-scale</v-icon>
+                  </v-avatar>
+                  {{issue.taskVolume}}
+                </v-chip>
+                <v-chip small text-small outlined class="mr-2" color="indigo" text-color="indigo">
+                  <v-avatar small left>
+                    <v-icon small>mdi-diamond-stone</v-icon>
+                  </v-avatar>
+                  {{issue.effectivity}}
+                </v-chip>
+
                 <v-chip small outlined text-small class="mr-2" color="indigo" text-color="indigo">
                   <v-avatar small left>
                     <v-icon small>mdi-account-circle</v-icon>
@@ -53,15 +72,31 @@
                 </v-chip>
                 <v-chip small text-small outlined class="mr-2" color="indigo" text-color="indigo">
                   <v-avatar small left>
-                    <v-icon small>mdi-diamond-stone</v-icon>
+                    <v-icon small>mdi-progress-check</v-icon>
                   </v-avatar>
-                  {{issue.effectivity}}
+                  {{issue.status}}
+                </v-chip>
+                <v-chip small text-small outlined class="mr-2" color="indigo" text-color="indigo">
+                  <v-avatar small left>
+                    <v-icon small>mdi-calendar-clock-outline</v-icon>
+                  </v-avatar>
+                  {{issue.executeDate}}
+                </v-chip>
+                <v-chip small text-small outlined class="mr-2" color="indigo" text-color="indigo">
+                  <v-avatar small left>
+                    <v-icon small>mdi-calendar-check-outline</v-icon>
+                  </v-avatar>
+                  {{issue.retroDate}}
                 </v-chip>
               </v-chip-group>
             </v-card-text>
             <v-card-text>
-              <div class="my-4 text-subtitle-1">{{issue.solution}}</div>
-              <div class="my-4 text-subtitle-1">{{issue.expect}}</div>
+              <div class="my-4 text-subtitle-1">課題概要：{{issueDetail}}</div>
+              <div class="my-4 text-subtitle-1">期待・学習：{{issue.expect}}</div>
+              <div class="my-4 text-subtitle-1">解決方法：{{issue.solution}}</div>
+              <div class="my-4 text-subtitle-1">メモ：{{issue.memo}}</div>
+              <div class="my-4 text-subtitle-1">実施方針：{{issue.direction}}</div>
+              <div class="my-4 text-subtitle-1">結果：{{issue.result}}</div>
             </v-card-text>
           </v-container>
         </v-card>
@@ -82,20 +117,20 @@ export default {
       selection: 1,
       headers: [
         { text: "課題タイトル", value: "issueName" }, //ToDo:追加済み
-        { text: "共有状況", value: "shareState" },
-        { text: "作業ボリューム", value: "taskVolume" },
+        { text: "共有状況", value: "shareState" }, //ToDo:追加済み
+        { text: "作業ボリューム", value: "taskVolume" }, //ToDo:追加済み
         { text: "効果見込み", value: "effectivity" }, //ToDo:追加済み
         { text: "優先度", value: "priority" }, //ToDo:追加済み
-        { text: "実施状況", value: "status" },
+        { text: "実施状況", value: "status" }, //ToDo:追加済み
         { text: "担当", value: "incharge" }, //ToDo:追加済み
         { text: "実施日", value: "executeDate" },
         { text: "振り返り日", value: "retroDate" },
-        { text: "課題概要", value: "issueDetail" },
+        { text: "課題概要", value: "issueDetail" }, //ToDo:追加済み
         { text: "解決方法", value: "solution" }, //ToDo:追加済み
-        { text: "期待", value: "expect" },
-        { text: "メモ", value: "memo" },
-        { text: "実施方針", value: "direction" },
-        { text: "結果", value: "result" },
+        { text: "期待", value: "expect" }, //ToDo:追加済み
+        { text: "メモ", value: "memo" }, //ToDo:追加済み
+        { text: "実施方針", value: "direction" }, //ToDo:追加済み
+        { text: "結果", value: "result" }, //ToDo:追加済み
         { text: "操作", value: "action", sortable: false }
       ],
       issues: []
