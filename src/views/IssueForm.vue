37140e1344e0e7e3f5616042c08b87ff793411df
大山和徳 <kazunori-oyama@tonichi-printing.co.jp>
2022-02-08 09:26:13 +0900

    datePickerの値を編集できなかったため修正

--- src/views/IssueForm.vue
+++ src/views/IssueForm.vue
@@ -71,7 +71,7 @@
                     v-on="on"
                   ></v-text-field>
                 </template>
-                <v-date-picker v-model="issue.executeDate" scrollable>
+                <v-date-picker v-model="executeDate" scrollable>
                   <v-spacer></v-spacer>
                   <v-btn text color="primary" @click="executeDateModal = false">Cancel</v-btn>
                   <v-btn text color="primary" @click="$refs.dialog.save(executeDate)">OK</v-btn>
@@ -94,7 +94,7 @@
                     v-on="on"
                   ></v-text-field>
                 </template>
-                <v-date-picker v-model="issue.retroDate" scrollable>
+                <v-date-picker v-model="retroDate" scrollable>
                   <v-spacer></v-spacer>
                   <v-btn text color="primary" @click="retroDateModal = false">Cancel</v-btn>
                   <v-btn text color="primary" @click="$refs.dialog2.save(retroDate)">OK</v-btn>
